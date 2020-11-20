5af252e0ef5bbc0b5d5884ec3e5a5a4ad149a37c
c121034t@icloud.com <c121034t@icloud.com>
2020-11-20 16:14:26 +0900

    rdfstoreにクエリしてjson結果を得るところまで

--- routes/search.js
+++ routes/search.js
@@ -1,22 +1,50 @@
 const express = require('express')
 const router = express.Router()
 
+const documentSearchQueryGenerator = require('../my_modules/documentSearchQueryGenerator.js')
+const fetch = require('node-fetch');
+
+const headers = {
+    'Content-Type': 'application/sparql-query',
+    'Accept': 'application/sparql-results+json',
+};
+
+const endpoint = 'http://localhost:3030/w3c-email/query';
+
 router.get('/', (req, res, next) => {
     var data = {
         title: 'ML Search',
-        content: "Input keyword"
+        content: "Input keyword",
     };
     res.render('search', data);
 });
 
-router.post('/result', (req, res, next) => {
-    var keyword_str = req.body['keyword'];
-    var keyword_list = keyword_str.split('\s');
-    var data = {
-        title: 'ML Search',
-        content: 'Search result about [' + keyword_list + '].' 
+router.post('/result', (req, res1, next) => {
+    let keyword = req.body['keyword'];
+    let query = documentSearchQueryGenerator(keyword);
+    let options = {
+        method: 'POST',
+        headers: headers,
+        body: query,
     };
-    res.render('search', data)
+    let data;
+    fetch(endpoint,options)
+        .then(res2 => {if (!res2.ok) {
+        // 200 系以外のレスポンスはエラーとして処理
+        throw new Error(`${res2.status} ${res2.statusText}`);
+        }
+        return res2.json();
+        })
+        .then(json => {
+            console.log(JSON.stringify(json));
+            data = {
+                title: 'ML Search',
+                content: 'Search result about [' + keyword + '].' ,
+                results: JSON.stringify(json),
+            };
+            res1.render('result', data)
+        })
+        .catch(err => console.error(err));
 });
 
 module.exports = router;
