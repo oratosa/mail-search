6f3b8c747e2809f62787a1a76db915e1d67075a2
c121034t@icloud.com <c121034t@icloud.com>
2020-11-27 15:33:49 +0900

    誰と誰が議論しているか（関連情報）の表示に成功

--- routes/search.js
+++ routes/search.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const ejs = require('ejs');
 
 const documentSearchQueryGenerator = require('../my_modules/documentSearchQueryGenerator.js');
 const personSearchQueryGenerator = require('../my_modules/personSearchQueryGenerator.js');
@@ -28,7 +29,7 @@ router.post('/result', (req, res1, next) => {
         headers: headers,
         body: query,
     };
-    let data;
+    // let data;
     fetch(endpoint,options)
         .then(res2 => {if (!res2.ok) {
         // 200 系以外のレスポンスはエラーとして処理
@@ -68,11 +69,58 @@ router.post('/result/api/person', (req, res1, next) => {
         .then(json => {
             console.log(JSON.stringify(json.head));
             console.log(JSON.stringify(json.results));
+            // test //
+            const head = json.head;
+            const results = json.results;
+            let template = `
+            <script>
+            jQuery(function($){
+                $("#relevant-people").DataTable({
+                    order: [],
+                });
+            });
+            </script>
+            <div>
+            <h2 class="text text-success"> 
+                Relevant People
+            </h2>
+                <table id="relevant-people" class="table table-bordered">
+                    <thead> 
+                        <tr> 
+                            <th data-field="fromWho"> 
+                                <% let fromWho = head.vars[0] %>
+                                <%= fromWho %> 
+                            </th> 
+                            <th data-field="toWho"> 
+                                <% let toWho = head.vars[1] %>
+                                <%= toWho %> 
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <% let rows = results.bindings; %>
+                        <% for(let i = 0; i < rows.length; i++){ %>
+                            <% let row = rows[i]; %>
+                            <% let fromWho = row.fromWho ?row.fromWho["value"] : null ; %>
+                            <% let toWho = row.toWho ? row.toWho["value"] : null ; %>
+                            <tr>
+                                <td><%= fromWho %></td>
+                                <td><%= toWho %></td>
+                            </tr>
+                        <% } %>
+                    </tbody> 
+                </table>
+            </div>
+            `;
+            const html = ejs.render(template, { head: head, results: results });
+            res1.send(html);
+            /* testのためコメントアウト
             data = {
                 head: json.head,
                 results: json.results,
             };
             res1.json(data);
+            */
         })
         .catch(err => console.error(err));
 });
