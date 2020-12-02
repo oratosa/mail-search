d6e0530eed0a414ac3f783e0717e059732cca39d
c121034t@icloud.com <c121034t@icloud.com>
2020-12-02 10:48:34 +0900

    文書集合中に出現したエンティティとそれらの文脈情報の数を表示する

--- routes/search.js
+++ routes/search.js
@@ -4,6 +4,8 @@ const ejs = require('ejs');
 
 const documentSearchQueryGenerator = require('../my_modules/documentSearchQueryGenerator.js');
 const personSearchQueryGenerator = require('../my_modules/personSearchQueryGenerator.js');
+const entitySearchQueryGenerator = require('../my_modules/entitySearchQueryGenerator.js');
+const filterDocumentByPeopleQueryGenerator = require('../my_modules/filterDocumentByPeopleQueryGenerator.js');
 const fetch = require('node-fetch');
 
 const headers = {
@@ -22,14 +24,71 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/result', (req, res1, next) => {
-    let keyword = req.body['keyword'];
-    let query = documentSearchQueryGenerator(keyword);
+    if (req.body['keyword']){
+        let keyword = req.body['keyword'];
+        let query = documentSearchQueryGenerator(keyword);
+        let options = {
+            method: 'POST',
+            headers: headers,
+            body: query,
+        };
+        fetch(endpoint,options)
+            .then(res2 => {if (!res2.ok) {
+            // 200 系以外のレスポンスはエラーとして処理
+                throw new Error(`${res2.status} ${res2.statusText}`);
+            }
+                return res2.json();
+            })
+            .then(json => {
+                console.log(JSON.stringify(json));
+                data = {
+                    title: 'ML Search',
+                    keyword: keyword,
+                    head: json.head,
+                    results: json.results,
+                };
+                res1.render('result', data)
+            })
+            .catch(err => console.error(err));
+    }else if (req.body['keyword_plus_people']){
+        let input = JSON.parse(req.body['keyword_plus_people']); // JSONにパースする必要ある
+        let keyword = input["keyword"];
+        let selected_cells = input['selected_cells'];
+        let query = filterDocumentByPeopleQueryGenerator(keyword, selected_cells);
+        let options = {
+            method: 'POST',
+            headers: headers,
+            body: query,
+        };
+        fetch(endpoint,options)
+            .then(res2 => {if (!res2.ok) {
+            // 200 系以外のレスポンスはエラーとして処理
+                throw new Error(`${res2.status} ${res2.statusText}`);
+            }
+                return res2.json();
+            })
+            .then(json => {
+                console.log(JSON.stringify(json));
+                data = {
+                    title: 'ML Search',
+                    keyword: keyword,
+                    head: json.head,
+                    results: json.results,
+                };
+                res1.render('result', data)
+            })
+            .catch(err => console.error(err));
+    }
+});
+
+router.post('/result/api/person', (req, res1, next) => {
+    let files = req.body['files'];
+    let query = personSearchQueryGenerator(files);
     let options = {
         method: 'POST',
         headers: headers,
         body: query,
     };
-    // let data;
     fetch(endpoint,options)
         .then(res2 => {if (!res2.ok) {
         // 200 系以外のレスポンスはエラーとして処理
@@ -38,21 +97,20 @@ router.post('/result', (req, res1, next) => {
             return res2.json();
         })
         .then(json => {
-            console.log(JSON.stringify(json));
+            console.log(JSON.stringify(json.head));
+            console.log(JSON.stringify(json.results));
             data = {
-                title: 'ML Search',
-                keyword: keyword,
                 head: json.head,
                 results: json.results,
             };
-            res1.render('result', data)
+            res1.json(data);
         })
         .catch(err => console.error(err));
 });
 
-router.post('/result/api/person', (req, res1, next) => {
+router.post('/result/api/entity', (req, res1, next) => {
     let files = req.body['files'];
-    let query = personSearchQueryGenerator(files);
+    let query = entitySearchQueryGenerator(files);
     let options = {
         method: 'POST',
         headers: headers,
