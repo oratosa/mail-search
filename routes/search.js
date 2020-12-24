e9b94489a79af23da900a0bf89563b40f65f5aac
c121034t@icloud.com <c121034t@icloud.com>
2020-12-24 11:39:38 +0900

    endpointをq3にした

--- routes/search.js
+++ routes/search.js
@@ -20,7 +20,7 @@ const headers = {
     'Accept': 'application/sparql-results+json',
 };
 
-const endpoint = 'http://localhost:3030/w3c-email-q3/query';
+const endpoint = 'http://localhost:3030/w3c-email/query';
 
 router.get('/', (req, res, next) => {
     res.render('search');
